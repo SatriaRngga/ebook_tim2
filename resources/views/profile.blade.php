5b3c72a91863c5cbf125b620ec8dd336501dd1a5
SatriaRngga <160713373+SatriaRngga@users.noreply.github.com>
2024-03-13 14:08:01 +0700

    dm

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html>
-<head>
-<title>Profile </title>
-</head>
-<body>
-<h1> Profile saya </h1>
-</body>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Ini Halaman Profile/h1>
+    </body>
 </html>
